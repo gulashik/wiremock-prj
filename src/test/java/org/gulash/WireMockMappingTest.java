afc7faeb60a4b9c1b354c8fef2ff607f24b632fd
Максим Гулько <gulashmail@gmail.com>
2026-04-05 21:15:47 +0300

    Working state

--- src/test/java/org/gulash/WireMockMappingTest.java
+++ src/test/java/org/gulash/WireMockMappingTest.java
@@ -16,11 +16,16 @@ public class WireMockMappingTest {
     private UserClient userClient;
 
     /**
-     * WireMockExtension по умолчанию ищет маппинги в src/test/resources/mappings.
+     * WireMockExtension ЯВНО настроен на поиск маппингов в src/test/resources/wiremock.
+     * ПО УМОЛЧАНИЮ (лучше так использовать)
+     * WireMock ищет файлы в src/test/resources (для маппингов - mappings, для тел - __files).
      */
     @RegisterExtension
     static WireMockExtension wireMock = WireMockExtension.newInstance()
-            .options(wireMockConfig().dynamicPort())
+            .options(wireMockConfig()
+                    .dynamicPort()
+                    // явно настраиваем путь от src/test/resources, но имеет смысл оставить по умолчанию
+                    .usingFilesUnderClasspath("wiremock")) // будет src/test/resources/wiremock/mappings
             .build();
 
     @BeforeEach
@@ -33,7 +38,7 @@ public class WireMockMappingTest {
     @DisplayName("Проверка получения пользователя через JSON маппинг (файловую заглушку)")
     void testGetUserFromMapping() throws IOException, InterruptedException {
         // Шаги теста:
-        // 1. Подготовка: WireMock автоматически загрузил маппинг из src/test/resources/mappings/user-mapping.json.
+        // 1. Подготовка: WireMock автоматически загрузил маппинг из src/test/resources/wiremock/mappings/user-mapping.json.
         //    WireMock знает, что возвращать, так как в этом JSON файле указано соответствие:
         //    при GET запросе на URL "/users/100" нужно вернуть статус 200 и конкретное тело JSON.
         User expectedUser = new User(100, "Mapping User");
@@ -48,15 +53,15 @@ public class WireMockMappingTest {
     @Test
     @DisplayName("Проверка получения пользователя через маппинг с использованием внешнего файла тела (bodyFileName)")
     /**
-     * Тест демонстрирует использование файла 'user-200.json' из папки src/test/resources/__files/
+     * Тест демонстрирует использование файла 'user-200.json' из папки src/test/resources/wiremock/__files/
      * в качестве тела ответа (bodyFileName) для WireMock-заглушки.
      */
     void testGetUserFromBodyFile() throws IOException, InterruptedException {
         // Шаги теста:
-        // 1. Подготовка: WireMock загрузил маппинг из src/test/resources/mappings/user-body-file-mapping.json.
+        // 1. Подготовка: WireMock загрузил маппинг из src/test/resources/wiremock/mappings/user-body-file-mapping.json.
         //    WireMock сопоставляет запрос по методу GET и URL "/users/200".
         //    В маппинге указан bodyFileName: "user-200.json", поэтому WireMock берет содержимое 
-        //    этого файла из папки src/test/resources/__files/ и возвращает его в теле ответа.
+        //    этого файла из папки src/test/resources/wiremock/__files/ и возвращает его в теле ответа.
         User expectedUser = new User(200, "Body File User");
 
         // 2. Действие: Вызываем метод клиента, который делает запрос на /users/200.
